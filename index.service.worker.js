bbd0a547ba410f7f3f7ec7884ac52847bd44b2ff
Firezac <erick.barros@uol.com.br>
2025-08-27 16:08:47 -0300

    BugCorrect And Load Screen On Activitys Path

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756315958|19867920201';
+const CACHE_VERSION = '1756321673|25583075648';
 /** @type {string} */
 const CACHE_PREFIX = 'AventurasSequenc-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
