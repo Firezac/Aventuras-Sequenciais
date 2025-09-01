e242d2fa5e5355e529868d1447163e6597a48275
Firezac <erick.barros@uol.com.br>
2025-09-01 15:13:54 -0300

    -

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756749411|27886980089';
+const CACHE_VERSION = '1756750421|28896529312';
 /** @type {string} */
 const CACHE_PREFIX = 'AventurasSequenc-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
