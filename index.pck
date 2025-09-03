70f62ced0e21711de92cf2e341082c813da8a8d9
Firezac <erick.barros@uol.com.br>
2025-09-03 13:47:12 -0300

    -

--- index.pck
+++ index.pck
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ab1c2d14c16938865e40889a04e6b2551988a68c6f018fc74b6f6b02c4ba7bb
-size 321338608
+oid sha256:a6f857a42a7b50c4938afb0ca005bc6076b492b80cc17be9ca5a53e5505de9ef
+size 321340064
--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756904546|72858027850';
+const CACHE_VERSION = '1756917970|11425327143';
 /** @type {string} */
 const CACHE_PREFIX = 'AventurasSequenc-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
