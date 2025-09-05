207ca8eef5a37ce0731819d5fb0830fe33eb0c9c
Firezac <erick.barros@uol.com.br>
2025-09-05 16:48:12 -0300

    -Heavy First Screens optimization

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757093817|10666918883';
+const CACHE_VERSION = '1757101604|18454274121';
 /** @type {string} */
 const CACHE_PREFIX = 'AventurasSequenc-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
